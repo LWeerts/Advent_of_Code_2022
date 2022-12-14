4d2e16f07e8b467a0b709062c3d88a94d374e063
LWeerts <lucweerts@hotmail.com>
2022-12-14 18:27:01 +0100

    Completed part 2

--- Excercises/03/rucksack_sorter.py
+++ Excercises/03/rucksack_sorter.py
@@ -6,17 +6,38 @@ def find_wrong_item(rucksack: str):
     return part_1 & part_2
 
 
-def main():
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    alphabet += alphabet.upper()
-    priorities = {k: v for k, v in zip(alphabet, range(1, 53))}
+def find_item_in_both_compartments(priorities: dict):
+
     total_priorities = 0
     with open("input.txt") as file:
         for line in file:
             wrong_item = find_wrong_item(line.strip()).pop()
             item_num = priorities[wrong_item]
             total_priorities += item_num
-    print(total_priorities)
+    print(f"part 1: {total_priorities}")
+
+
+def find_item_in_3_rucksacks(priorities: dict):
+    with open("input.txt") as file:
+        total_badge_num = 0
+        while True:
+            rucksack_1 = set(file.readline().strip())
+            rucksack_2 = set(file.readline().strip())
+            rucksack_3 = set(file.readline().strip())
+            badge = rucksack_1 & rucksack_2 & rucksack_3
+            if len(badge) == 0:
+                break
+            badge_num = priorities[badge.pop()]
+            total_badge_num += badge_num
+    print(f"part 2: {total_badge_num}")
+
+
+def main():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet += alphabet.upper()
+    priorities = {k: v for k, v in zip(alphabet, range(1, 53))}
+    find_item_in_both_compartments(priorities)
+    find_item_in_3_rucksacks(priorities)
 
 
 if __name__ == "__main__":
