da495ea6695329075348e763674f9a85e2cac93e
LWeerts <lucweerts@hotmail.com>
2022-12-23 07:48:44 +0100

    Solved ex 6, part 1

--- Excercises/06/start_of_packet_finder.py
+++ Excercises/06/start_of_packet_finder.py
@@ -3,10 +3,11 @@
 def main():
     with open("input.txt") as file:
         datastream = file.readline()
-    for ix in range(3, len(datastream)):
+    for ix in range(0, len(datastream) - 3):
         bit = datastream[ix: ix + 4]
         if len(set(bit)) == 4:
-            print(f"Found start_of_packet at index {ix + 1}")
+            print(f"Found start_of_packet at index {ix + 4}")
+            print(bit)
             quit()
 
 
