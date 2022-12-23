0b2762e351d5b844b3a770a4ee21960aa69b9c99
Kimvana <66371226+Kimvana@users.noreply.github.com>
2022-12-23 16:56:38 +0100

    Completed day 06

--- Excercises/06/start_of_packet_finder.py
+++ Excercises/06/start_of_packet_finder.py
@@ -4,9 +4,9 @@ def main():
     with open("input.txt") as file:
         datastream = file.readline()
     for ix in range(0, len(datastream) - 3):
-        bit = datastream[ix: ix + 4]
-        if len(set(bit)) == 4:
-            print(f"Found start_of_packet at index {ix + 4}")
+        bit = datastream[ix: ix + 14]
+        if len(set(bit)) == 14:
+            print(f"Found start_of_packet at index {ix + 14}")
             print(bit)
             quit()
 
