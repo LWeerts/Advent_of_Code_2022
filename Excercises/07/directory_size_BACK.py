9c77a4a8a9097218999843d055f20d8d84132823
Kimvana <66371226+Kimvana@users.noreply.github.com>
2022-12-24 10:12:57 +0100

    Work on ex 07

---
+++ Excercises/07/directory_size_BACK.py
@@ -0,0 +1,23 @@
+def main():
+    filetree = {"/": {}}
+    parent_dir = []
+    current_dir = filetree["/"]
+    with open("input.txt") as file:
+        for line in file:
+            if line.startswith("$ cd .."):
+                current_dir = parent_dir.pop()
+            elif line.startswith("$ cd /"):
+                pass
+            elif line.startswith("$ cd"):
+                parent_dir.append(current_dir)
+                current_dir = current_dir[line.split[-1]]
+            elif line.startswith("$ ls"):
+                continue
+            elif line.startswith("dir"):
+                current_dir[line.split[-1]] = {}
+            else: 
+                pass
+
+# bla 
+if __name__ == "__main__":
+    main()
