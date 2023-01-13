7ae61c061ca2951acc0e15032a97e6d034d1b380
LWeerts <lucweerts@hotmail.com>
2023-01-13 14:10:22 +0100

    completed day 14 part 2

--- Excercises/14/sand_falls.py
+++ Excercises/14/sand_falls.py
@@ -2,13 +2,17 @@ import numpy as np
 
 
 class PlayingField():
-    def __init__(self) -> None:
+    def __init__(self, part_1_2) -> None:
         # self.field = np.zeros((200, 1000), dtype=bool)
         self.field = np.full((200, 1000), " ", dtype="U1")
-        self.read_map()
-        self.field[-1, :][self.field[-1, :] == " "] = "A"
+        self.highest_y = self.read_map()
+        if part_1_2 == 1:
+            self.field[-1, :][self.field[-1, :] == " "] = "A"
+        elif part_1_2 == 2:
+            self.field[self.highest_y + 2, :] = "#"
 
     def read_map(self):
+        highest_y = 0
         with open("Excercises/14/input.txt") as file:
             for line in file:
                 split_line = line.split(" -> ")
@@ -17,10 +21,13 @@ class PlayingField():
                     coord_x, coord_y = point.split(",")
                     # Flip coordinates so it matches numpy array indices
                     point_list.append((int(coord_y), int(coord_x)))
+                    if int(coord_y) > highest_y:
+                        highest_y = int(coord_y)
                 for pair in zip(point_list[:-1], point_list[1:]):
                     min_y, max_y = sorted((pair[0][0], pair[1][0]))
                     min_x, max_x = sorted((pair[0][1], pair[1][1]))
                     self.field[min_y: max_y + 1, min_x: max_x + 1] = "#"
+        return highest_y
 
 
 class Sand():
@@ -45,6 +52,8 @@ class Sand():
         drop_result = (0, 500)  # Spawn location
         while drop_result[0] != "End":
             drop_result = (0, 500)
+            if self.field[0, 500] == "o":
+                break
             while drop_result[0] != "Rest":
                 # Move to the next position
                 drop_result = self.next_move(*drop_result)
@@ -58,8 +67,8 @@ class Sand():
 
 
 def main():
-    field = PlayingField()
-    print(field.field[:11, 493:505])
+    field = PlayingField(2)
+    print(field.field[:12, 493:505])
     sandfall = Sand(field)
     sandfall.drop_sand()
     print(sandfall.num_of_sand())
