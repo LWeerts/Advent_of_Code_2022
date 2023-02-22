df345333583387af00c658bc807e560752db9307
Kimvana <66371226+Kimvana@users.noreply.github.com>
2023-02-22 15:19:43 +0100

    edit find_path() and added sort_flow()
    find_path() now uses a dictionary to keep track of which valves have been visited and how far away they are.
    sort_flow() was added to sort a list of (valvename, flow) by flow, to easily find the best valves to open.

--- Excercises/16/pressure_release.py
+++ Excercises/16/pressure_release.py
@@ -12,11 +12,12 @@ class Valve():
 
 class PlayingField():
     def __init__(self) -> None:
-        self.valve_dict: dict[str, Valve] = self.parse_input()
+        self.valve_dict = self.parse_input()
+        self.flow_list = self.sort_flow()
         self.turn = 0
         self.position = "AA"
 
-    def parse_input(self) -> dict[Valve]:
+    def parse_input(self) -> dict[str, Valve]:
         valve_dict = {}
         with open("Excercises/16/input_test.txt") as file:
             for line in file:
@@ -29,26 +30,57 @@ class PlayingField():
                 valve_dict[name] = Valve(name, flow, tunnels)
         return valve_dict
 
-    def find_path(self, target: str) -> None:
-        # connections = self.valve_dict[self.position].tunnels
-        distance_list = [[self.position]]
+    def sort_flow(self) -> list[str, int]:
+        """For finding the valves with the largest flow.
+
+        Returns:
+            list: tuples of valvename and flow, sorted by flow.
+        """
+        output = []
+        for name, valve in self.valve_dict.items():
+            flow = valve.flow
+            output.append((name, flow))
+        output.sort(key=lambda valve: valve[1], reverse=True)
+        return output
+
+    def find_path(self, target: str) -> int:
+        """Finds shortest path in a simple network.
+
+        Uses a dictionary to store each node and its distance.
+        All nodes in the 'outer shell' are searched for new connections
+        which are then added into the dictionary. 
+
+        Returns:
+            int: length of the path to the target
+        """
+        distance_dict = {self.position: 0}
         distance = 0
         while distance < 1000:
-            if target in distance_list[distance]:
+            if target in distance_dict:
                 break
-            for valve in distance_list[distance]:
+            for valve, dist in distance_dict.items():
+                if dist != distance:
+                    continue
                 connections = self.valve_dict[valve].tunnels
-                if distance + 1 == len(distance_list):
-                    distance_list.append([])
-                distance_list[distance + 1].extend(connections)
+                for tunnel in connections:
+                    if tunnel in distance_dict:
+                        continue
+                    distance_dict[tunnel] = distance + 1
 
             distance += 1
+        return distance_dict[target]
+
+    def calc_score(self, target: str) -> int:
+        # Calculate what total pressure will be released from target valve
+        pass
 
 
 def main():
     field = PlayingField()
-    print(field.valve_dict)
-    print(field.valve_dict["AA"])
+    # print(field.valve_dict)
+    # print(field.valve_dict["AA"])
+    # print(field.flow_list)
+
 
 
 if __name__ == "__main__":
